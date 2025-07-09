5219b09be364aa14f922e3246b947f80036b5b86
NicolasB <nicolasgamer908@gmail.com>
2025-07-08 20:27:27 -0500

    Cambios Preguntas Frec

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -11,18 +11,41 @@ const Contact = () => {
         <div className="Cont_Prin">
           
           <div className='Cont_Derecho'>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora iure officia soluta quibusdam corporis. Rerum consequuntur ratione soluta, eligendi facere quos aut odit debitis tempora dolorum deserunt enim nam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora iure officia soluta quibusdam corporis. Rerum consequuntur ratione soluta, eligendi facere quos aut odit debitis tempora dolorum deserunt enim nam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora iure officia soluta quibusdam corporis. Rerum consequuntur ratione soluta, eligendi facere quos aut odit debitis tempora dolorum deserunt enim nam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora iure officia soluta quibusdam corporis. Rerum consequuntur ratione soluta, eligendi facere quos aut odit debitis tempora dolorum deserunt enim nam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora iure officia soluta quibusdam corporis. Rerum consequuntur ratione soluta, eligendi facere quos aut odit debitis tempora dolorum deserunt enim nam.</p>
+            <div className='Preguntas'>
+              <p><strong>¿cuales son sus horarios de atencion?</strong></p>
+              <p>Lunes -  Viernes: 11am - 7:30pm <br /> Sabados: 12:00am - 7:30pm <br />Domingos y Festivos: Cerrados <br/>Porfavor llega mínimo 40 minutos antes de la hora de cierre.</p>
+            </div>
+            <div className='Preguntas'>
+              <p><strong>¿Puedo cambiar de vestido?</strong></p>
+              <p>Aceptamos cambio de vestido siempre y cuando <br /> sea para la misma fecha. <br /> Debes de venir a hacer el cambio en nuestro establecimiento <br /> mínimo con un día de anticipación.</p>
+            </div>
+            <div className='Preguntas'>
+              <p><strong>¿Cuál es su política de cancelación?</strong></p>
+              <p>Si, puedes cancelar tu cita con tres dias de anticipación. <br /> Si no cancelas tu cita con tres dias de anticipación, <br /> perderás el depósito que hayas realizado.</p>
+            </div>
+            <div className='Preguntas'>
+              <p><strong>¿Puedo mandar a hacer mi vestido?</strong></p>
+              <p>Nosotros no confeccionamos vestidos, <br /> todos nuestros vestidos son importados y <br />no hacemos a la medida</p>
+            </div>
           </div>
+
           <div className='Cont_Izquierdo'>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, doloribus dolore iusto vitae quibusdam suscipit velit dicta animi et sapiente natus earum sunt architecto perspiciatis doloremque libero? Quod, minus quam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, doloribus dolore iusto vitae quibusdam suscipit velit dicta animi et sapiente natus earum sunt architecto perspiciatis doloremque libero? Quod, minus quam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, doloribus dolore iusto vitae quibusdam suscipit velit dicta animi et sapiente natus earum sunt architecto perspiciatis doloremque libero? Quod, minus quam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, doloribus dolore iusto vitae quibusdam suscipit velit dicta animi et sapiente natus earum sunt architecto perspiciatis doloremque libero? Quod, minus quam.</p>
-            <p>-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, doloribus dolore iusto vitae quibusdam suscipit velit dicta animi et sapiente natus earum sunt architecto perspiciatis doloremque libero? Quod, minus quam.</p>
+            <div className='Preguntas'>
+              <p><strong>¿Cómo puedo devolver los vestidos?</strong></p>
+              <p>El vestido se debe de devolver en nuestro <br /> local. Puedes hacerlo tu u otra persona de <br />confianza a quien le podamos regresar tu <br /> deposito.</p>
+            </div>
+            <div className='Preguntas'>
+              <p><strong>¿Puedo reservar un vestido sin pagar?</strong></p>
+              <p>No, para reservar un vestido debes realizar un abono. <br /> El abono se descontará del total del alquiler del vestido.</p>
+            </div>
+            <div className='Preguntas'>
+              <p><strong>¿Tienen servicio de domicilio?</strong></p>
+              <p>No manejamos servicio a domicilio, puedes recoger <br /> tu vestido y entregarlo personalmente o un <br /> tercero en nuestro Showroom</p>
+            </div>
+            <div className='Preguntas'>
+              <p><strong>¿Ustedes arreglan el vestido?</strong></p>
+              <p>Nosotros ajustamos el vestido a tu altura y <br /> podemos entallarlo siempre y cuando esto <br /> no afecte la integridad del vestido. Hay <br />algunas telas que se dañan muy fácilmente. </p>
+            </div>
           </div>
 
         </div>
